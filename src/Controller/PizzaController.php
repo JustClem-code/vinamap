6f9e780ed5c2bfac387ab4db909ccb9616daf051
Clément Lemarié <clemarie@vinatis.com>
2024-01-28 16:44:03 +0100

    wip

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -3,29 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 use App\Entity\VictoriousPizza;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
 
 class PizzaController extends AbstractController
 {
     #[Route('/pizza', name: 'app_pizza')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PizzaController.php',
-        ]);
-    }
-
-    #[Route('/pizza', name: 'app_pizza')]
     public function createPizza(EntityManagerInterface $entityManager): Response
     {
         $pizza = new VictoriousPizza();
-        $pizza->setName('Reine');
+        $pizza->setName('Rebochonnade');
 
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($pizza);
